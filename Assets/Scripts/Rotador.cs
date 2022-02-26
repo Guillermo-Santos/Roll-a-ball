3b571193cc0b0c9d40a788bbd18970f69675ddd5
Guillermo Santos <santosguillermo360@outlook.com>
2022-02-26 18:06:14 -0400

    Subida de codigo del juego

---
+++ Assets/Scripts/Rotador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Rotador : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Rota el elemento una cantidad diferente en cada dirección y encada intervalo de tiempo
+        transform.Rotate(new Vector3(30, 0, 0) * Time.deltaTime);
+    }
+}
